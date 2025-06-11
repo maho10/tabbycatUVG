4e7f6951013b4e4a384386aa03ff796a2e93c162
maho10 <69826606+maho10@users.noreply.github.com>
2025-06-10 23:23:56 -0600

    Update forms.py

--- tabbycat/results/forms.py
+++ tabbycat/results/forms.py
@@ -907,7 +907,7 @@ class PerAdjudicatorBallotSetForm(ScoresMixin, BaseBallotSetForm):
         """Adds the speaker score fields:
          - <side>_score_a#_s#,  one for each score
         """
-        super().create_score_fields()
+        BaseBallotSetForm.create_score_fields(self)
         for side, pos in product(self.sides, self.positions):
             scorefield = ReplyScoreField if (pos == self.reply_position) else SubstantiveScoreField
             for adj in self.adjudicators:
